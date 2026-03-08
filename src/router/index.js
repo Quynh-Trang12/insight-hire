f98d3e9f53f154099df0e4b97400b5779611dcbf
Quynh Trang <quynhtranglengoc@gmail.com>
2026-03-08 21:24:50 +0700

    Version 1.4-update router

--- src/router/index.js
+++ src/router/index.js
@@ -19,42 +19,43 @@
 import { createRouter, createWebHistory } from "vue-router";
 import JobExplorer from "../views/JobExplorer.vue";
 
-const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+const routes = [
+  {
+    path: "/",
+    redirect: "/jobs",
+  },
+  {
+    path: "/jobs",
+    name: "jobs-layout",
+    component: JobExplorer,
+    children: [
+      {
+        path: "",
+        name: "job-overview",
+        component: () => import("../components/jobs/JobOverview.vue"),
+      },
+      {
+        path: ":id",
+        name: "job-detail",
+        component: () => import("../components/jobs/JobDetail.vue"),
+      },
+    ],
+  },
+  {
+    path: "/apply",
+    name: "job-application",
+    component: () => import("../views/ApplicationView.vue"),
+  },
+  {
+    path: "/todos",
+    name: "todo-list",
+    component: () => import("../views/TodosView.vue"),
+  },
+];
 
-  routes: [
-    {
-      path: "/",
-      redirect: "/jobs",
-    },
-    {
-      path: "/jobs",
-      name: "jobs-layout",
-      component: JobExplorer,
-      children: [
-        {
-          path: "",
-          name: "job-overview",
-          component: () => import("../components/jobs/JobOverview.vue"),
-        },
-        {
-          path: ":id",
-          name: "job-detail",
-          component: () => import("../components/jobs/JobDetail.vue"),
-        },
-      ],
-    },
-    {
-      path: "/apply",
-      name: "job-application",
-      component: () => import("../views/ApplicationView.vue"),
-    },
-    {
-      path: "/todos",
-      name: "todo-list",
-      component: () => import("../views/TodosView.vue"),
-    },
-  ],
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
 });
 
 export default router;
